2899a7e6dcdc18cb7ea758d174bd2cabb4017ba5
Netu0 <osvaldo.beltrame.neto@gmail.com>
2025-08-27 00:03:18 -0300

    Ambiente Spring Boot configurado

---
+++ desafio/desafio/src/test/java/com/oswaldo/desafio/DesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oswaldo.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
